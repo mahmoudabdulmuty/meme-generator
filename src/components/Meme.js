6bdd5b17c27d06be0afe58776502f665e7b15d18
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-22 09:23:28 +0200

    adding meme text

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,12 +2,11 @@ import { useState } from 'react';
 import memesData from '../memesData';
 
 export default function Meme() {
-	const [meme, setMemeImage] = useState({
+	const [meme, setMeme] = useState({
 		topText: '',
 		bottomText: '',
 		randomImage: ''
 	});
-
 	const [allMemeImages] = useState(memesData);
 
 	function getMemeImage() {
@@ -16,22 +15,49 @@ export default function Meme() {
 		const min = 0;
 		const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
 		const memUrl = memes[randomNum].url;
-		setMemeImage((prevMeme) => {
+		setMeme((prevMeme) => {
 			return {
 				...prevMeme,
 				randomImage: memUrl
 			};
 		});
 	}
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setMeme((prevMeme) => {
+			return {
+				...prevMeme,
+				[name]: value
+			};
+		});
+	};
 	return (
 		<main className="main-container">
 			<section className="meme">
-				<input className="meme__text" type="text" placeholder="first" />
-				<input className="meme__text" type="text" placeholder="second" />
+				<input
+					name="topText"
+					value={meme.topText}
+					className="meme__text"
+					type="text"
+					placeholder="Top Text"
+					onChange={handleChange}
+				/>
+				<input
+					name="bottomText"
+					value={meme.bottomText}
+					className="meme__text"
+					type="text"
+					placeholder="Bottom Text"
+					onChange={handleChange}
+				/>
 				<button onClick={getMemeImage} className="meme__btn">
 					Get a new meme image 🖼
 				</button>
-				<img className="meme-photo" src={meme.randomImage} alt="" />
+				<div className="meme">
+					<img className="meme-photo" src={meme.randomImage} alt="" />
+					<h2 className="meme--text top">{meme.topText}</h2>
+					<h2 className="meme--text bottom">{meme.bottomText}</h2>
+				</div>
 			</section>
 		</main>
 	);
